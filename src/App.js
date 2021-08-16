10e581752399b1f8a996f8a5c5f1c7cb58b866c4
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-08-15 21:22:14 -0300

    testing array

--- src/App.js
+++ src/App.js
@@ -1,86 +1,96 @@
 import React from 'react';
 
-const titulo = <h1>Título do site</h1>
+// const numero = parseInt(Math.random() * 100);
+// const ativo = true;
 
-    // const numero = parseInt(Math.random() * 100);
-    // const ativo = true;
+// const estiloP = {
+//     color: "tomato",
 
-    // const estiloP = {
-    //     color: "tomato",
+// }
 
-    // }
+// const carro = {
+//     marca: "Ford",
+//     rodas: "4"
+// }
 
-    // const carro = {
-    //     marca: "Ford",
-    //     rodas: "4"
-    // }
+// function mostrarNome(sobrenome){
+//     return `Célio ${sobrenome}`
+// }
 
-    // function mostrarNome(sobrenome){
-    //     return `Célio ${sobrenome}`
-    // }
-
-    // return <> 
-    //     {titulo}
-    //     <p className={ativo ? 'ativo' : 'null'}>Numéro aleatório {numero / 2}</p>
-    //     <p style={estiloP}>{mostrarNome("Viana")}</p>
-        
-    //     <p>
-    //         {new Date().getFullYear()}
-    //     </p>
-    //     <p>{carro.marca}</p>
-    //  </>
+// return <> 
+//     {titulo}
+//     <p className={ativo ? 'ativo' : 'null'}>Numéro aleatório {numero / 2}</p>
+//     <p style={estiloP}>{mostrarNome("Viana")}</p>
+    
+//     <p>
+//         {new Date().getFullYear()}
+//     </p>
+//     <p>{carro.marca}</p>
+//  </>
 
 
-    // // return (
-    // //     // <a className="ativo" href="https://github.com/celiovjunior" title="Repositories">Meu GitHub</a>
-    // //     <>
-    // //         <label htmlFor="nome">Nome</label>
-    // //         <input type="text" id="nome"/>
-    // //     </>
-    // // );
+// // return (
+// //     // <a className="ativo" href="https://github.com/celiovjunior" title="Repositories">Meu GitHub</a>
+// //     <>
+// //         <label htmlFor="nome">Nome</label>
+// //         <input type="text" id="nome"/>
+// //     </>
+// // );
 
-    const luana = {
-        cliente: 'Luana',
-        idade: 27,
-        compras: [
-          { nome: 'Notebook', preco: 'R$ 2500' },
-          { nome: 'Geladeira', preco: 'R$ 3000' },
-          { nome: 'Smartphone', preco: 'R$ 1500' },
-        ],
-        ativa: true,
-      };
-      
-      const mario = {
-        cliente: 'Mario',
-        idade: 31,
-        compras: [
-          { nome: 'Notebook', preco: 'R$ 2500' },
-          { nome: 'Geladeira', preco: 'R$ 3000' },
-          { nome: 'Smartphone', preco: 'R$ 1500' },
-          { nome: 'Guitarra', preco: 'R$ 3500' },
-        ],
-        ativa: false,
-      };
+// const luana = {
+//     cliente: 'Luana',
+//     idade: 27,
+//     compras: [
+//       { nome: 'Notebook', preco: 'R$ 2500' },
+//       { nome: 'Geladeira', preco: 'R$ 3000' },
+//       { nome: 'Smartphone', preco: 'R$ 1500' },
+//     ],
+//     ativa: true,
+//   };
+  
+//   const mario = {
+//     cliente: 'Mario',
+//     idade: 31,
+//     compras: [
+//       { nome: 'Notebook', preco: 'R$ 2500' },
+//       { nome: 'Geladeira', preco: 'R$ 3000' },
+//       { nome: 'Smartphone', preco: 'R$ 1500' },
+//       { nome: 'Guitarra', preco: 'R$ 3500' },
+//     ],
+//     ativa: false,
+//   };
 
-      const App = () => {
-        const dados = luana;
+//   const App = () => {
+//     const dados = mario;
 
-        const total = dados.compras
-        .map((item) => Number(item.preco.replace('R$ ', '')))
-        .reduce((a, b) => a + b);
+//     const total = dados.compras
+//     .map((item) => Number(item.preco.replace('R$ ', '')))
+//     .reduce((a, b) => a + b);
 
-        return <>
-        <h1>Dados do cliente</h1>
-        <p>Nome: {dados.cliente}</p>
-        <p>Idade: {dados.idade}</p>
-        <p>Situação: {' '}
-            <span style={{color: dados.ativa ? 'green' : 'red'}}>
-                {dados.ativa ? 'Ativa' : 'Inativa'}
-            </span>
-        </p>
-        
-        <p>Total:  R$ {total}</p>
-        {total > 1000 ? <p>Gastou muito</p> : <p>Dentro do limite</p>}
-        </>;
-      };
+//     return <>
+//     <h1>Dados do cliente</h1>
+//     <p>Nome: {dados.cliente}</p>
+//     <p>Idade: {dados.idade}</p>
+//     <p>Situação: {' '}
+//         <span style={{color: dados.ativa ? 'green' : 'red'}}>
+//             {dados.ativa ? 'Ativa' : 'Inativa'}
+//         </span>
+//     </p>
+    
+//     <p>Total:  R$ {total}</p>
+//     {total > 1000 ? <p>Gastou muito</p> : <p>Dentro do limite</p>}
+//     </>;
+//   };
+
+const App = () => {
+  const filmes = ['filme 1', 'filme 2', 'filme 3', 'filme 4']
+
+  return (
+    <ul>
+      {filmes.map((filme) => (
+        <li key={filme}>{filme}</li>
+      ))}
+    </ul>
+  )
+}
 export default App;
