3f58281b3bdb2e65724c9337a9ee05d6dd27fcc1
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-09-17 20:17:29 -0300

    react use effect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
+
 
 const App = () => {
-  const [modal, setModal] = React.useState(false);
+  const [contar, setContar] = React.useState(0);
+  
+  const [dados, setDados] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`https://ranekapi.origamid.dev/json/api/produto/notebook`)
+    .then((response) => response.json())
+    .then((json) => setDados(json))
+  }, [])
 
   return (
     <div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      {dados && (<div>
+        <h1>{dados.nome}</h1>
+        <p>{dados.preco}</p>
+      </div>)}
     </div>
-  )  
-
-  // return <div></div>
+  )
 }
-
 export default App;
