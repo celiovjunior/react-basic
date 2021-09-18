371714b711a97c450e2848a0c425c7f443a69a37
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-09-17 21:02:35 -0300

    react use effect and use state

--- src/Produto.js
+++ src/Produto.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-const Produto = () => {
-    React.useEffect(() => {
-         function handleScroll(event) {
-            console.group(event)
-         }
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        }
-    }, []);
+const Produto = ({produto}) => {
+    const [dados, setDados] = React.useState(null);
 
-    return (
-        <div style={{height: '200vh'}}>
-            <p>Meu produto</p>
-        </div>
-    )
+    React.useEffect(() => {
+        if(produto !== null)
+        fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then(r => r.json())
+        .then((json) => setDados(json))
+    }, [])
+
+    if(dados === null) return null;
+    return <div>
+        <h1>{dados.nome}</h1>
+        <p>{dados.preco}</p>
+    </div>
 }
 
-export default Produto
+export default Produto;
