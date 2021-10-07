e452384cffeef489b73c2109ff708e5c3e0b9c94
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-10-07 18:24:40 -0300

    use ref and time out

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
 const App = () => {
-  const [comentarios, setComentarios] = React.useState([]);
+  const [carrinho, setCarrinho] = React.useState(0);
+  
+  const [notificacao, setNotificacao] = React.useState(null);
 
-  const [input, setInput] = React.useState();
-
-  const inputElement = React.useRef();
+  const timeoutRef = React.useRef();
 
   function handleClick() {
-    setComentarios([...comentarios, input]);
-    setInput('');
-    inputElement.current.focus()
+    setCarrinho(carrinho + 1)
+    setNotificacao('Item adicionado ao carrinho')
+
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() =>{
+      setNotificacao(null)
+    }, 1000)
   }
 
   return <div>
-    <ul>
-      {comentarios.map(comentario => <li key={comentario}>{ comentario }</li>)}
-    </ul>
-      <input type="text" ref={inputElement} value={input} onChange={({target}) => setInput(target.value)}/>
-      <button onClick={handleClick}>Enviar</button>
-      <br/>
+    <p>{notificacao}</p>
+    <button onClick={handleClick}>Adicionar Carrinho</button>
+    <p>{carrinho}</p>
   </div>
 }
 export default App;
