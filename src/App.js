379ee06d3a4386aa1200706f0e4142f54d005d2c
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-10-07 18:13:40 -0300

    using react use ref

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 const App = () => {
-  const [comentarios, setComentarios] = React.useState(['teste', 'teste 2']);
+  const [comentarios, setComentarios] = React.useState([]);
 
-  const [input, setInput] = React.useState('teste');
+  const [input, setInput] = React.useState();
+
+  const inputElement = React.useRef();
+
+  function handleClick() {
+    setComentarios([...comentarios, input]);
+    setInput('');
+    inputElement.current.focus()
+  }
 
   return <div>
     <ul>
       {comentarios.map(comentario => <li key={comentario}>{ comentario }</li>)}
-      <input type="text" value={input} onChange={({target}) => setInput(target.value)}/>
     </ul>
+      <input type="text" ref={inputElement} value={input} onChange={({target}) => setInput(target.value)}/>
+      <button onClick={handleClick}>Enviar</button>
+      <br/>
   </div>
 }
 export default App;
