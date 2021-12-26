8efab72c6056789824c6da7a7c9044767ca8b966
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-26 01:39:15 -0300

    setando valores em forma de objetos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 const App = () => {
-  const [nome, setNome] = React.useState('')
-  const [email, setEmail] = React.useState('')
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: ''
+  })
+
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(nome, event)
+    console.log(form)
+  }
+
+  function handleChange({target}) {
+    const {id, value} = target
+    console.log(id, value)
+    setForm({
+      ...form,[id]: value
+    })
   }
   
   return <form onSubmit={handleSubmit}>
     <label htmlFor="nome">Nome</label>
-    <input name="nome" id="nome" type="text" value={nome} onChange={(event) => setNome(event.target.value)} />
+    <input name="nome" id="nome" type="text" value={form.nome} onChange={handleChange} />
+    {/* {form.nome} */}
     <label htmlFor="email">Email</label>
-    <input name="email" id="email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+    <input name="email" id="email" type="email" value={form.email} onChange={handleChange} />
+    {/* {form.email} */}
     <button>Enviar</button>
   </form>
 }
