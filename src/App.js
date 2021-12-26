180d69beffe13edacc98a131aae6aebdf6e34a82
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-26 01:33:01 -0300

    input e values

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const App = () => {
   const [nome, setNome] = React.useState('')
+  const [email, setEmail] = React.useState('')
 
-  return <form>
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(nome, event)
+  }
+  
+  return <form onSubmit={handleSubmit}>
     <label htmlFor="nome">Nome</label>
-    <input id="nome" type="text" value={nome} onChange={(event) => setNome(event.target.value)} />
+    <input name="nome" id="nome" type="text" value={nome} onChange={(event) => setNome(event.target.value)} />
+    <label htmlFor="email">Email</label>
+    <input name="email" id="email" type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+    <button>Enviar</button>
   </form>
 }
 
