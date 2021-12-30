3ea50f7205348a4e728c1985d60c0a8880aef8c7
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-30 15:08:22 -0300

    criando o componente Radio button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Input from './Form/Input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
 
   return(
     <form>
+      <h2>Cores</h2>
+      <Radio
+       options={['Azul', 'Branco']}
+       value={ cor } 
+       setValue={ setCor }
+      />
+
       <Select
        options={['smartphone', 'tablet']} 
        value={produto} 
