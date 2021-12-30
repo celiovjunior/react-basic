ca066459c735ece9916b9c0391781cc90498da4b
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2021-12-30 16:39:28 -0300

    estilizacao de componenets com animacoes em css

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
 import React from 'react';
-import Input from './Form/Input';
-import Radio from './Form/Radio';
-import Select from './Form/Select';
+import Produto from './Produto';
+import './App.css';
 
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
+  const [ativar, setAtivar] = React.useState(false)
 
-  return(
-    <form>
-      <h2>Cores</h2>
-      <Radio
-       options={['Azul', 'Branco']}
-       value={ cor } 
-       setValue={ setCor }
-      />
-
-      <Select
-       options={['smartphone', 'tablet']} 
-       value={produto} 
-       setValue={setProduto}
-      />
-     
-      <Input
-       id="nome"
-       label="Nome"
-       value={nome}
-       setValue={setNome}
-       required
-      />
-
-      <Input
-       id="email" 
-       label="Email" 
-       value={email} 
-       setValue={setEmail}
-      />
-
-      <button>Enviar</button>
-    </form>
-  )
+ return <div>
+   <button onClick={() => setAtivar(!ativar)}>Ativar</button>
+   {ativar && <Produto /> }
+ </div>
 }
 
 export default App;
