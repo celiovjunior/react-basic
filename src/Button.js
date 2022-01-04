98d7ead1cfafaf9e8418c8092686908fd1481264
Célio Viana da Silva Júnior <celio_cjr@hotmail.com>
2022-01-04 03:59:48 -0300

    usando defaultProps para setar propriedades fisicas, mas que podem ser sobrescritas

--- src/Button.js
+++ src/Button.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 
 const Button = (props) => {
     return (
-        <button style={{width: `${props.width}px`, height:`${props.width/3}px`}}>{props.children}</button>
+        <button style={{ margin: props.margin, width: `${props.width}px`, height:`${props.width/3}px`}}>{props.children}</button>
     )
 };
+
+Button.defaultProps = {
+    margin: '10px'
+}
 
 Button.propTypes = {
     width: PropTypes.number.isRequired,
